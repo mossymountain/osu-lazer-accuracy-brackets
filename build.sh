2ca818550226abc35ab7e5f967c2a695170d1690
mossymountain <osmo.hyttinen@gmail.com>
2023-07-31 14:34:56 +0300

    Make build.sh tell you it isn't always needed

--- build.sh
+++ build.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 cd "$(dirname "$(readlink -fm "$0")")"
-exec npm install
+npm install
+echo $'\n\x1b[32mIf that seemed like it worked, \x1b[1myou don'"'t need to "$'\x1b[22mrun '"$0"$' again\x1b[m unless you update.\x1b[m'
